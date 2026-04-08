ea98a6e635be2b7bf79aadb44ed5029c818ba692
AtharvaTaras <atharvataras26@gmail.com>
2026-04-08 11:25:23 +0530

    moved pre/post proc funcs

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,52 +1,6 @@
 import re
 from typing import Optional
 
-def simple_preprocess(text:str) -> str:
-	# Performs basic text preprocessing
-	
-	pairs = [
-	['--', ' '], 
-	['__', ''], 
-	['“', '"'], 
-	['”', '"'],
-	]
-
-	for pair in pairs:
-		text = re.sub(pair[0], pair[1], text)
-
-	text = re.sub(r'([!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~])', r' \1 ', text) # Add spaces around punctuation
-	text = re.sub(r'(\d{1})', r'\1 ', text)                                   # Add spaces after digits
-	text = re.sub(r'\s+', ' ', text)                                          # Replace multiple spaces with a single space
-
-	return text.lower()
-
-
-def simple_postprocess(text:str) -> str:
-	text = re.sub(r'\s([!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~])', r'\1', text)
-	text = re.sub(r'\s(\d{1})', r'\1', text)
-	return text
-
-
-def advanced_preprocess(text:str) -> str:
-	# Performs more advanced text preprocessing
-	pairs = [
-	['--', ' '], 
-	['__', ''], 
-	['“', '"'], 
-	['”', '"'],
-	]
-
-	for pair in pairs:
-		text = re.sub(pair[0], pair[1], text)
-
-	text = re.sub(r'([!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~])', r' \1 ', text) # Add spaces around punctuation
-	text = re.sub(r'(\d+)', r'\1 ', text)                                   # Add spaces after digits
-	text = re.sub(r'\n', ' <newline> ', text)                                   # Replace newlines with a token
-	# text = re.sub(r'([A-Z])', '<caps> \1', text)                              # Add a token before capital letters
-	text = re.sub(r'\b([A-Z][a-z]*)\b', lambda m: ' <caps> ' + m.group(1).lower(), text) 
-
-	return text.lower()
-
 
 # def advanced_postprocess(text:str) -> str:
 # 	tokens = text.split()
@@ -95,64 +49,6 @@ def advanced_preprocess(text:str) -> str:
 # 	# Remove spaces around slashes
 # 	text = re.sub(r'\s*/\s*', '/', text)
 # 	return text
-
-
-def advanced_postprocess(text: str) -> str:
-    tokens = text.split()
-    result = []
-    i = 0
-
-    # --- 1. Handle <caps> ---
-    while i < len(tokens):
-        if tokens[i] == "<caps>" and i + 1 < len(tokens):
-            next_token = tokens[i + 1]
-
-            if re.match(r'^[a-zA-Z]+$', next_token):
-                result.append(next_token.capitalize())
-            else:
-                result.append(next_token)
-
-            i += 2
-        else:
-            result.append(tokens[i])
-            i += 1
-
-    text = " ".join(result)
-
-    # --- 2. REMOVE leftover <caps> EARLY ---
-    text = re.sub(r'\b<caps>\b\s*', '', text)
-
-    # --- 3. Restore newlines ---
-    text = re.sub(r'\s*<newline>\s*', '\n', text)
-
-    # --- 4. Fix contractions ---
-    text = re.sub(r"([a-zA-Z])'\s([a-zA-Z])", r"\1'\2", text)
-
-    # --- 5. Remove space BEFORE punctuation ---
-    text = re.sub(r'\s+([!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~])', r'\1', text)
-
-    # --- 6. Ensure space AFTER punctuation ---
-    text = re.sub(r'([,:;.!?])([^\s\n])', r'\1 \2', text)
-
-    # --- 7. Fix brackets ---
-    text = re.sub(r'\[\s+', '[', text)
-    text = re.sub(r'\s+\]', ']', text)
-    text = re.sub(r'([^\s])(\[)', r'\1 \2', text)
-    text = re.sub(r'\](\w)', r'] \1', text)
-
-    # --- 8. Fix # spacing ---
-    text = re.sub(r'\s+#\s+', ' #', text)
-    text = re.sub(r'([^\s])#', r'\1 #', text)
-
-    # --- 9. Fix URLs ---
-    text = re.sub(r'(\w)\.\s+(\w)', r'\1.\2', text)
-    text = re.sub(r'\s*/\s*', '/', text)
-
-    # --- 10. Normalize spaces (preserve newlines) ---
-    text = re.sub(r'[ \t]+', ' ', text)
-    text = re.sub(r' *\n *', '\n', text)
-
-    return text.strip()
 
 
 def generate_vocabulary(text:str) -> dict:
